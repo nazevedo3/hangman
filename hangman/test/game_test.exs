7738af404f55a7fe82e617017fef0f8c307523e7
nazevedo3 <nazevedo3@gmail.com>
2021-03-16 16:58:36 -0700

    turned hangman into a OTP Server

--- hangman/test/game_test.exs
+++ hangman/test/game_test.exs
@@ -5,85 +5,88 @@ defmodule GameTest do
 
   test "new_game returns structure" do
     game = Game.new_game()
+
     assert game.turns_left == 7
     assert game.game_state == :initializing
     assert length(game.letters) > 0
   end
 
-  test "verify all letters are lower case" do
-    game = Game.new_game()
-    word = Enum.into(game.letters, "")
-    assert word == String.downcase(word, :ascii)
-  end
-
   test "state isn't changed for :won or :lost game" do
-    for state <- [:won, :lost] do
+    for state <- [ :won, :lost ] do
       game = Game.new_game() |> Map.put(:game_state, state)
-      assert ^game = Game.make_move(game, "x")
+      assert  {^game, _} = Game.make_move(game, "x")
     end
   end
 
-  test "first occurence of letter is not already used" do
+  test "first occurrence of letter is not already used" do
     game = Game.new_game()
-    game = Game.make_move(game, "x")
+    { game, _ } = Game.make_move(game, "x")
     assert game.game_state != :already_used
   end
 
-  test "second occurence of letter is not already used" do
+  test "second occurrence of letter is not already used" do
     game = Game.new_game()
-    game = Game.make_move(game, "x")
+    { game, _ } = Game.make_move(game, "x")
     assert game.game_state != :already_used
-    game = Game.make_move(game, "x")
+    { game, _ } = Game.make_move(game, "x")
     assert game.game_state == :already_used
   end
 
   test "a good guess is recognized" do
     game = Game.new_game("wibble")
-    game = Game.make_move(game, "w")
+    { game, _ } = Game.make_move(game, "w")
     assert game.game_state == :good_guess
     assert game.turns_left == 7
   end
 
-  test "a guessed word is a wong game" do
+  test "a guessed word is a won game" do
     game = Game.new_game("wibble")
-    game = Game.make_move(game, "w")
+    { game, _ } = Game.make_move(game, "w")
     assert game.game_state == :good_guess
     assert game.turns_left == 7
-    game = Game.make_move(game, "i")
+    { game, _ } = Game.make_move(game, "i")
     assert game.game_state == :good_guess
     assert game.turns_left == 7
-    game = Game.make_move(game, "b")
+    { game, _ } = Game.make_move(game, "b")
     assert game.game_state == :good_guess
     assert game.turns_left == 7
-    game = Game.make_move(game, "l")
+    { game, _ } = Game.make_move(game, "l")
     assert game.game_state == :good_guess
     assert game.turns_left == 7
-    game = Game.make_move(game, "e")
+    { game, _ } = Game.make_move(game, "e")
     assert game.game_state == :won
     assert game.turns_left == 7
   end
 
-  test "a lost game is recognized" do
-    game = Game.new_game("w")
-    game = Game.make_move(game, "a")
+  test "bad guess is recognized" do
+    game = Game.new_game("wibble")
+    { game, _ } = Game.make_move(game, "x")
     assert game.game_state == :bad_guess
     assert game.turns_left == 6
-    game = Game.make_move(game, "b")
+  end
+
+  test "lost game is recognized" do
+    game = Game.new_game("w")
+    { game, _ } = Game.make_move(game, "a")
+    assert game.game_state == :bad_guess
+    assert game.turns_left == 6
+    { game, _ } = Game.make_move(game, "b")
     assert game.game_state == :bad_guess
     assert game.turns_left == 5
-    game = Game.make_move(game, "c")
+    { game, _ } = Game.make_move(game, "c")
     assert game.game_state == :bad_guess
     assert game.turns_left == 4
-    game = Game.make_move(game, "d")
+    { game, _ } = Game.make_move(game, "d")
     assert game.game_state == :bad_guess
     assert game.turns_left == 3
-    game = Game.make_move(game, "e")
+    { game, _ } = Game.make_move(game, "e")
     assert game.game_state == :bad_guess
     assert game.turns_left == 2
-    game = Game.make_move(game, "f")
+    { game, _ } = Game.make_move(game, "f")
     assert game.game_state == :bad_guess
     assert game.turns_left == 1
-    game = Game.make_move(game, "g")
+    { game, _ } = Game.make_move(game, "g")
     assert game.game_state == :lost
   end
+
 end
